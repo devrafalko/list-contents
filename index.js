bc41dba0ab7c1e926f04ef1de491e9cab208900e
Pawel Rafalko <dev.rafalko@gmail.com>
2017-09-08 22:31:53 +0200

    Promises replaced by callback based asynchronouse function chains invocations with the 'move-on' package.

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
-/* global Promise */
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const args = require('typeof-arguments');
 const cliColor = require('cli-color');
+const moveOn = require('move-on');
 const error = cliColor.red;
-const warn = cliColor.bgYellow.black;
+const warn = cliColor.bgYellow.blue;
 
 module.exports = function(getPath,callback){
   args(arguments,['string','function'],(o)=>{
     var err = new TypeError(warn('list-contents') + ': ' + error(o.message));
     throw err;
   });
-  var msgError = new Error(warn('list-contents') + ': ' + error('Could not get the access to the contents of the given directory path.'));
-  var dirs = [];
-  var files = [];
+  var userContext = {dirs:[], files:[], path:getPath, error:null };
 
-  run(getPath)
-  .then(()=>callback({error:null,files:files,dirs:dirs,path:getPath}))
-  .catch((err)=>callback({error:err,files:files,dirs:dirs,path:getPath}));
+  moveOn([explore],userContext,callback,(u,e)=>{
+    callback({dirs:[], files:[], path:getPath, error:e });
+  });
 
-  function run(pth){
-    return new Promise((resolve,reject)=>{
-      getContent(pth)
-      .then((o)=>getDirsAndFiles(o))
-      .then(()=>resolve())
-      .catch((err)=>reject(err));
-    });
-  }
-
-  function getContent(pth){
-    return new Promise((resolve,reject)=>{
-      fs.readdir(pth,(err,content)=>{
-        if(err) {
-          reject(msgError);
-        } else {
-          resolve({pth:pth,content:content});
-        }
-      });
-    });
-  }
-
-  function getDirsAndFiles(o){
-    return new Promise((resolve,reject)=>{
-      var promises = [];
-      for(let i in o.content){
-        let p = path.resolve(o.pth,o.content[i]);
-        let rel = path.relative(getPath,p);
-        var promise = new Promise((resolve,reject)=>{
-          fs.lstat(p,(err,stats)=>{
-            if(err){
-              reject(msgError);
-              return;
-            } else {
-              if(stats.isDirectory()){
-                dirs.push(rel);
-                run(p).then(()=>{
-                  resolve();
-                });
-              } else {
-                files.push(rel);
-                resolve();
-              }
-            }
+  function explore(resolve,reject,r){
+    var relative = typeof r==='undefined' ? '':r;
+    var abs = path.resolve(this.path,relative);
+    fs.readdir(abs,(err,contents)=>{
+      if(err) reject(new Error(warn('list-contents') + ': ' + error(`Could not get the access to the '${parseSlashes(abs)}' path.`)));
+      if(!err){
+        var contentsIter = 0;
+        if(!contents.length) resolve();
+        for(let i in contents){
+          checkItem.call(this,relative,contents[i],(isDir,relative)=>{
+            if(isDir) explore.call(this,iter,reject,relative);
+            if(!isDir) iter();
           });
-        });      
-        promises.push(promise);
+        }
       }
-      Promise.all(promises).then(()=>resolve(),(err)=>reject(err));
+        function iter(){
+          if(++contentsIter===contents.length) resolve();
+        }
     });
+  }
+
+  function checkItem(r,item,resolve,reject){
+    var absolute = path.resolve(this.path,r,item);
+    var relative = path.join(r,item);
+    fs.stat(absolute,(err,stats)=>{
+      if(err) reject(new Error(warn('list-contents') + ': ' + error(`Could not get the access to the '${parseSlashes(absolute)}' path.`)));
+      if(!err){
+        var exists = err === null,
+            isFile = stats && stats.isFile(),
+            isDir = stats && stats.isDirectory();
+        if(exists && isFile) this.files.push(parseSlashes(relative));
+        if(exists && isDir) this.dirs.push(parseSlashes(relative));
+        resolve(isDir,relative);
+      }
+    });
+  }
+  
+  function parseSlashes(path){
+    return path.replace(/\\/g,'/');
   }
 };
